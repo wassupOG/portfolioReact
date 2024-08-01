94fad1b7fe60e7f4b0c9e3405a706ad92a3ce206
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2024-08-01 22:05:05 +0300

    upd

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -7,7 +7,7 @@ import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
 import EngineeringIcon from "@mui/icons-material/Engineering";
 
 const STACK =
-    "React, Node, GraphQL, REST, TypeScript, JavaScript, Prisma, Jest, Zustand, Redux, Redux Toolkit, Next.js, NestJS, Express.js, MySQL, PostgreSQL, CSS, SCSS, Tailwind, turborepo, ApolloClient, TanStack Query (React Query), axios, react-admin, ApolloServer, materialUI, shadcnUI";
+    "React, Node, GraphQL, REST, TypeScript, JavaScript, Docker, Prisma, Jest, Zustand, Redux, Redux Toolkit, Next.js, NestJS, Express.js, MySQL, PostgreSQL, CSS, SCSS, Tailwind, turborepo, ApolloClient, TanStack Query (React Query), axios, react-admin, ApolloServer, materialUI, shadcnUI";
 const RESUME_LINK = "https://disk.yandex.com/i/g7pIy6pgVgLrBg";
 
 const ruContents = [
@@ -43,7 +43,7 @@ const ruContents = [
                 <div className="flex-row-small-gap">
                     <WorkHistoryIcon />
                     <div>
-                        <b>Middle Fullstack React Разработчик</b>, Ноябрь 2023 — по настоящее время
+                        <b>Middle Fullstack React Разработчик</b>, ноябрь 2023 — по настоящее время
                     </div>
                 </div>
                 <div>
@@ -66,9 +66,50 @@ const ruContents = [
                         </div>
                         <div>— Разработка и доработка архитектуры сервисов, реализация бизнес-логики приложений</div>
                         <div>— Написание тестов (Jest)</div>
+                        <div>— Работа с Docker</div>
                         <div style={{ marginTop: "10px" }}>
-                            <b>Задействованный стек:</b> React, JavaScript, TypeScript, Node, GraphQL, PostgreSQL,
-                            Prisma, Jest, react-admin, MUI
+                            <b>Задействованный стек:</b> React, JavaScript, TypeScript, Docker, Node, GraphQL,
+                            PostgreSQL, Prisma, Jest, react-admin, MUI
+                        </div>
+                    </div>
+                </div>
+            </Paper>,
+            <Paper className="flex-column-small-gap" variant="outlined" sx={{ p: 1, borderRadius: 3, mt: 1 }}>
+                <div className="flex-row-small-gap">
+                    <BusinessIcon />
+                    <Link href="https://englishful.ru/" target="_blank">
+                        englishful
+                    </Link>
+                </div>
+                <div className="flex-row-small-gap">
+                    <WorkHistoryIcon />
+                    <div>
+                        <b>Fullstack Разработчик</b>, сентябрь 2021 — декабрь 2023
+                    </div>
+                </div>
+                <div>
+                    <div className="flex-row-small-gap">
+                        <EngineeringIcon />
+                        <div>Обязанности:</div>
+                    </div>
+                    <div className="flex-column-no-gap">
+                        <div style={{ marginBottom: "10px" }}>
+                            Разработка с нуля, деплой, SEO оптимизация и поддержание онлайн платформы для изучения
+                            английского языка englishful со следующим функционалом:
+                        </div>
+                        <div>— Аутентификация, идентификация, управление доступом на основе ролей</div>
+                        <div>— Онлайн пробники ЕГЭ (устная и письменная часть)</div>
+                        <div>
+                            — Словарь с озвучкой слов с помощью SpeechSynthesis API, а также её кросс-платформенной
+                            адаптацией, возможностью распределять слова по темам, тренировать слова с помощью карточек,
+                            общим прогрессом изучения слов, а также в разрезе по темам
+                        </div>
+                        <div>— Личный кабинет ученика с учебным планом и индикаторами прогресса обучения</div>
+                        <div>— Различные тесты с автопроверкой и сохранением результата в личном кабинете</div>
+                        <div>— Библиотека с теорией</div>
+                        <div style={{ marginTop: "10px" }}>
+                            <b>Задействованный стек:</b> HTML, чистый JavaScript без фреймворков, SCSS для стилей,
+                            Python, Django, MySQL
                         </div>
                     </div>
                 </div>
@@ -170,13 +211,55 @@ const enContents = [
                             logic
                         </div>
                         <div>— Code testing (Jest)</div>
+                        <div>— Working with Docker</div>
                         <div style={{ marginTop: "10px" }}>
-                            <b>Stack used:</b> React, JavaScript, TypeScript, Node, GraphQL, PostgreSQL, Prisma, Jest,
+                            <b>Stack used:</b> React, JavaScript, TypeScript, Docker, Node, GraphQL, PostgreSQL, Prisma, Jest,
                             react-admin, MUI
                         </div>
                     </div>
                 </div>
             </Paper>,
+             <Paper className="flex-column-small-gap" variant="outlined" sx={{ p: 1, borderRadius: 3, mt: 1 }}>
+             <div className="flex-row-small-gap">
+                 <BusinessIcon />
+                 <Link href="https://englishful.ru/" target="_blank">
+                     englishful
+                 </Link>
+             </div>
+             <div className="flex-row-small-gap">
+                 <WorkHistoryIcon />
+                 <div>
+                     <b>Fullstack Developer</b>, September 2021 — December 2023
+                 </div>
+             </div>
+             <div>
+                 <div className="flex-row-small-gap">
+                     <EngineeringIcon />
+                     <div>Responsibilities:</div>
+                 </div>
+                 <div className="flex-column-no-gap">
+                     <div style={{ marginBottom: "10px" }}>
+                     Development from scratch, deployment, SEO optimization and maintenance of an online platform for learning English with the following functionality:
+                     </div>
+                     <div>— Authentication, identification, role-based access control</div>
+                     <div>— English State Exam mock tests (speaning & writing)</div>
+                     <div>
+                         — Dictionary with pronunciation of words using SpeechSynthesis API, as well as its cross-platform
+
+adaptation, the ability to distribute words by topics, train words using cards,
+
+general progress in learning words, as well as by topic
+                     </div>
+                     <div>— Student's personal cabinet with curriculum and learning progress indicators</div>
+                     <div>— Various tests with automatic checking and saving the result in your personal account</div>
+                     <div>— Library with theory</div>
+                     <div style={{ marginTop: "10px" }}>
+                         <b>Stack used:</b> HTML, pure JavaScript without frameworks, SCSS for styling,
+                         Python, Django, MySQL
+                     </div>
+                 </div>
+             </div>
+         </Paper>,
         ],
     },
     {
